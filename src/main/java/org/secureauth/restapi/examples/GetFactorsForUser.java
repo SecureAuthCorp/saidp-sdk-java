c397c6c83592c86d266b55f44147aeea9900449d
Lei Ding <lding@secureauth.com>
2015-10-29 14:48:31 -0700

    Allows empty spaces in userIds. Always log stack trace in case of exceptions. Make sure that no new exceptions during log attempt.

---
+++ src/main/java/org/secureauth/restapi/examples/GetFactorsForUser.java
@@ -0,0 +1,81 @@
+package org.secureauth.restapi.examples;
+
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import org.secureauth.sarestapi.SAAccess;
+import org.secureauth.sarestapi.data.Factors;
+import org.secureauth.sarestapi.data.FactorsResponse;
+import org.secureauth.sarestapi.data.IPEval;
+import org.secureauth.sarestapi.data.ResponseObject;
+
+
+/**
+ * @author rrowcliffe@secureauth.com
+ * <p>
+ *     SAAccess is a class that allows access to the SecureAuth REST API. The intention is to provide an easy method to access
+ *     the Secureauth Authentication Rest Services.
+ * </p>
+ *
+ * <p>
+ * Copyright 2015 SecureAuth Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * </p>
+ */
+public class GetFactorsForUser {
+
+    //Define our User Variables
+    private static String user = "Test User";
+    private static String password = "test";
+    private static String otp = "";
+
+    //Required for connectivity to Appliance
+    private static String applianceHost = "user-identity.com";
+    private static String appliancePort = "443";
+    private static boolean applianceSSL = true;
+    private static String realm = "secureauth998";
+    private static String applicationID = "66ecde134bf343a99c0e8586328bb346";
+    private static String applicationKey = "3983de13b7c5cf39bc952c7599243507c4f0f8784817e7861892b7f5b141f530";
+
+
+    public static void main(String[] args) throws MalformedURLException, URISyntaxException{
+
+        //Create Instance of SAAccess Object
+        SAAccess saAccess = new SAAccess(applianceHost,appliancePort,applianceSSL,realm, applicationID, applicationKey);
+
+        System.out.println("Start Test++++++++++++++++++");
+        //Grab all available Factors for a user
+//        FactorsResponse factorsResponse = getFactors(saAccess, user);
+        
+        System.out.println(new StringBuilder().append("...").append((String)null));
+        System.out.println("End Test++++++++++++++++++++");
+
+    }
+
+    private static FactorsResponse getFactors(SAAccess saAccess,String userid){
+        //Return Factors
+        FactorsResponse factorsResponse = saAccess.factorsByUser(userid);
+        if(!factorsResponse.getStatus().equalsIgnoreCase("invalid")){
+            System.out.println("FACTORS +++++++++++++++++\n" + factorsResponse.toString());
+            System.out.println("END FACTORS++++++++++++++");
+            }else{
+            System.out.println("Failed to get factors " + factorsResponse.getMessage());
+        }
+        return factorsResponse;
+    }
+
+}
