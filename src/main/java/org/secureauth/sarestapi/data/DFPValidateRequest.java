af55a74a34e251c78cbfd595ba144a6e0e8b8662
sa-rrowcliffe <rrowcliffe@secureauth.com>
2016-01-17 20:31:44 -0800

    New classes supporting DFP

---
+++ src/main/java/org/secureauth/sarestapi/data/DFPValidateRequest.java
@@ -0,0 +1,67 @@
+package org.secureauth.sarestapi.data;
+
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @author rrowcliffe@secureauth.com
+ *
+ *
+Copyright (c) 2015, SecureAuth
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**/
+
+@XmlRootElement
+@JsonSerialize(include=JsonSerialize.Inclusion.NON_NULL)
+public class DFPValidateRequest {
+
+    private String user_id;
+    private String host_address;
+    private DFP fingerprint;
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getHost_address() {
+        return host_address;
+    }
+
+    public void setHost_address(String host_address) {
+        this.host_address = host_address;
+    }
+
+    public DFP getFingerprint() {
+        return fingerprint;
+    }
+
+    public void setFingerprint(DFP fingerprint) {
+        this.fingerprint = fingerprint;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("\t").append("User ID:").append(user_id);
+        stringBuilder.append("\n\t").append("Host Address:").append(host_address);
+        stringBuilder.append("\n\t\t").append("FingerPrint:").append(fingerprint);
+        return stringBuilder.toString();
+    }
+}
---
+++ src/main/java/org/secureauth/sarestapi/data/DFPValidateResponse.java
@@ -0,0 +1,107 @@
+package org.secureauth.sarestapi.data;
+
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @author rrowcliffe@secureauth.com
+ *
+ *
+Copyright (c) 2015, SecureAuth
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**/
+
+@XmlRootElement
+@JsonSerialize(include=JsonSerialize.Inclusion.NON_NULL)
+public class DFPValidateResponse {
+
+    private String fingerprint_id;
+    private String fingerprint_name;
+    private double score;
+    private double match_score;
+    private double update_score;
+    private String status;
+    private String message;
+
+    public String getFingerprint_id() {
+        return fingerprint_id;
+    }
+
+    public void setFingerprint_id(String fingerprint_id) {
+        this.fingerprint_id = fingerprint_id;
+    }
+
+    public String getFingerprint_name() {
+        return fingerprint_name;
+    }
+
+    public void setFingerprint_name(String fingerprint_name) {
+        this.fingerprint_name = fingerprint_name;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    public double getMatch_score() {
+        return match_score;
+    }
+
+    public void setMatch_score(double match_score) {
+        this.match_score = match_score;
+    }
+
+    public double getUpdate_score() {
+        return update_score;
+    }
+
+    public void setUpdate_score(double update_score) {
+        this.update_score = update_score;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("\t").append("FingerPrint ID:").append(fingerprint_id);
+        stringBuilder.append("\n\t").append("FingerPrint Name:").append(fingerprint_name);
+        stringBuilder.append("\n\t").append("Score:").append(score);
+        stringBuilder.append("\n\t").append("Match Score:").append(match_score);
+        stringBuilder.append("\n\t").append("Update Score:").append(update_score);
+        stringBuilder.append("\n\t").append("Status:").append(status);
+        stringBuilder.append("\n\t").append("Message:").append(message);
+        return stringBuilder.toString();
+    }
+}
