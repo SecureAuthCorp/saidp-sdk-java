3ba94c0c5538d6e670ae5dd8b124992d672c2073
sa-rrowcliffe <rrowcliffe@secureauth.com>
2016-04-20 21:47:33 -0700

    Added Additional Tests

--- src/test/java/org/secureauth/restapi/test/TestRestAPI.java
+++ src/test/java/org/secureauth/restapi/test/TestRestAPI.java
@@ -2,10 +2,7 @@ package org.secureauth.restapi.test;
 
 
 import org.secureauth.sarestapi.SAAccess;
-import org.secureauth.sarestapi.data.Factors;
-import org.secureauth.sarestapi.data.FactorsResponse;
-import org.secureauth.sarestapi.data.IPEval;
-import org.secureauth.sarestapi.data.ResponseObject;
+import org.secureauth.sarestapi.data.*;
 
 
 /**
@@ -35,30 +32,31 @@ public class TestRestAPI {
 
     //Define our User Variables
     private static String user = "user";
-    private static String password = "password";
-    //private static String otp = "033994";
+    private static String password = "xxxxxxxxx";
+    //private static String otp = "584233";
 
     //Required for connectivity to Appliance
-    private static String applianceHost = "example.domain.com";
+    private static String applianceHost = "host.domain.com";
     private static String appliancePort = "443";
     private static boolean applianceSSL = true;
     private static String realm = "secureauth1";
-    private static String applicationID = "5c1904cd5965480981c03a3e0db022b2";
-    private static String applicationKey = "416968246ce72d55ec3c6b1e91e81671e3a89349f71d79cd801c2c97e1494aca";
-    /*
-    private static String applicationID = "2c30646d6ec64d6bae351c0728cf594d";
-    private static String applicationKey = "6f03fdc29ff4dc17b95067bba0b68009d269193f77663d8a0a63c6a07079d626";
-    */
+    private static String applicationID = "....................";
+    private static String applicationKey = "......................";
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         //Create Instance of SAAccess Object
-        SAAccess saAccess = new SAAccess(applianceHost,appliancePort,applianceSSL,realm, applicationID, applicationKey);
+        SAAccess saAccess = new SAAccess(applianceHost, appliancePort, applianceSSL, realm, applicationID, applicationKey);
 
         System.out.println("Start Test++++++++++++++++++");
 
+        // Get Java Script Source URL
+        getJSSrc(saAccess);
+
+
         //Validate User
         testUser(saAccess, user);
+
 
         //Validate User & Password
         testPassword(saAccess,user,password);
@@ -67,64 +65,93 @@ public class TestRestAPI {
         FactorsResponse factorsResponse = getFactors(saAccess, user);
 
         //Loop through Factors and test OTP Delivery
-        for(Factors factor : factorsResponse.getFactors()){
+        for (Factors factor : factorsResponse.getFactors()) {
 
             //Validate OATH Token (Hard, Soft Tokens)
-            /*
+/*
             if(factor.getType().equalsIgnoreCase("oath")){
                testOath(saAccess,otp,factor.getId());
             }
 */
 
+
             //TestPush Delivery
             if(factor.getType().equalsIgnoreCase("push")){
                 testPush(saAccess,factor.getId());
             }
+
+
+            //Test Push to Accept
+            try {
+                if(factor.getType().equalsIgnoreCase("push")){
+                    for (String capability : factor.getCapabilities()){
+                        if (capability.equalsIgnoreCase("push_accept")) {
+                            testPushToAccept(saAccess,user,factor.getId(),"192.168.2.192");
+                        }
+                    }
+                }
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
             //Test Delivery by All Registered Phones
 
             if(factor.getType().equalsIgnoreCase("phone")) {
                 for (String capability : factor.getCapabilities()) {
                     //Test SMS Delivery
                     if (capability.equalsIgnoreCase("sms")) {
-                        testSmsOTP(saAccess, user, factor.getId());
+                      if(factor.getId().equalsIgnoreCase("phone2")) {
+                          testSmsOTP(saAccess, user, factor.getId());
+                      }
                     }
 
                     //Test Telephony Delivery
                     if (capability.equalsIgnoreCase("call")) {
-                        testPhoneOTP(saAccess, user, factor.getId());
+                        if(factor.getId().equalsIgnoreCase("phone2")){
+                            testPhoneOTP(saAccess, user, factor.getId());
+                        }
+
                     }
                 }
             }
-            /*
+
+/*
             //Test email delivery
             if(factor.getType().equalsIgnoreCase("email")){
                 testEmailOTP(saAccess, user, factor.getId());
             }
-
+*/
             //Test KBQ / KBA
-            /*
+
             if(factor.getType().equalsIgnoreCase("kbq")){
                 if(factor.getId().equalsIgnoreCase("KBQ1")){
-                    testKBQ(saAccess, user, "SB", factor.getId());
+                    testKBQ(saAccess, user, "zzzzzzzzzzzzzz", factor.getId());
                 }
             }
-            */
+
         }
 
-        //Grab IPViking data based on user and IP Address
-        testIP(saAccess, user,"116.211.0.90");
+        //Grab Threat Feed data based on user and IP Address
+        testIP(saAccess, user,"162.247.72.201");
+
+        //Test Adaptive Auth
+        testAdaptiveAuth(saAccess,user,"162.247.72.201");
+
+        //Access Histories Test
+        testAccessHistories(saAccess,user,"162.247.72.201");
 
         System.out.println("End Test+++++++++++++++++++");
 
     }
 
 
-    private static ResponseObject testUser(SAAccess saAccess,String userid){
+    private static ResponseObject testUser(SAAccess saAccess, String userid) {
         //Validate the User Exists
         ResponseObject responseObject = saAccess.validateUser(userid);
-        if(responseObject != null){
+        if (responseObject != null) {
 
-            if(responseObject.getStatus().equalsIgnoreCase("found")){
+            if (responseObject.getStatus().equalsIgnoreCase("found")) {
                 System.out.println("We matched User");
 
                 System.out.println(responseObject.toString());
@@ -136,9 +163,9 @@ public class TestRestAPI {
         return responseObject;
     }
 
-    private static ResponseObject testPassword(SAAccess saAccess, String userid, String password){
+    private static ResponseObject testPassword(SAAccess saAccess, String userid, String password) {
         ResponseObject passObject = saAccess.validateUserPassword(userid, password);
-        if(passObject != null){
+        if (passObject != null) {
 
             System.out.println("Password Test+++++++");
             System.out.println(passObject.toString());
@@ -147,60 +174,110 @@ public class TestRestAPI {
         return passObject;
     }
 
-    private static ResponseObject testPhoneOTP(SAAccess saAccess,String userid, String factorId){
+    private static ResponseObject testPhoneOTP(SAAccess saAccess, String userid, String factorId) {
         ResponseObject phoneOTP = saAccess.deliverOTPByPhone(userid, factorId);
         System.out.println("PHONE OTP is: " + phoneOTP.getOtp());
         return phoneOTP;
     }
 
-    private static ResponseObject testSmsOTP(SAAccess saAccess,String userid, String factorId){
+    private static ResponseObject testSmsOTP(SAAccess saAccess, String userid, String factorId) {
         ResponseObject smsOTP = saAccess.deliverOTPBySMS(userid, factorId);
         System.out.println("SMS OTP is: " + smsOTP.getOtp());
         return smsOTP;
     }
 
-    private static ResponseObject testEmailOTP(SAAccess saAccess, String userid, String factorId){
-        ResponseObject emailOTP = saAccess.deliverOTPByEmail(userid,factorId);
+    private static ResponseObject testEmailOTP(SAAccess saAccess, String userid, String factorId) {
+        ResponseObject emailOTP = saAccess.deliverOTPByEmail(userid, factorId);
         System.out.println("Email OTP is: " + emailOTP.getOtp());
         return emailOTP;
     }
 
-    private static ResponseObject testKBQ(SAAccess saAccess,String userid,String answer,String factorId){
-                System.out.println("TEST KBQ:  " + factorId);
-                ResponseObject kba = saAccess.validateKba(userid,answer,factorId);
-                System.out.println(kba.toString());
-                System.out.println("END KBQ +++++++++++");
+    private static ResponseObject testKBQ(SAAccess saAccess, String userid, String answer, String factorId) {
+        System.out.println("TEST KBQ:  " + factorId);
+        ResponseObject kba = saAccess.validateKba(userid, answer, factorId);
+        System.out.println(kba.toString());
+        System.out.println("END KBQ +++++++++++");
 
         return kba;
     }
 
 
-    private static FactorsResponse getFactors(SAAccess saAccess,String userid){
+    private static FactorsResponse getFactors(SAAccess saAccess, String userid) {
         //Return Factors
         FactorsResponse factorsResponse = saAccess.factorsByUser(userid);
-        if(!factorsResponse.getStatus().equalsIgnoreCase("invalid")){
+        System.out.println(factorsResponse.getMessage());
+        if (!factorsResponse.getStatus().equalsIgnoreCase("invalid")) {
             System.out.println("FACTORS +++++++++++++++++\n" + factorsResponse.toString());
             System.out.println("END FACTORS++++++++++++++");
-            }
+        }
         return factorsResponse;
     }
 
-    private static ResponseObject testOath(SAAccess saAccess, String otp, String factorId){
-        ResponseObject responseObject = saAccess.validateOath(user,otp,factorId);
+    private static ResponseObject testOath(SAAccess saAccess, String otp, String factorId) {
+        ResponseObject responseObject = saAccess.validateOath(user, otp, factorId);
+        System.out.println("Start OATH Test+++++++++++++++");
         System.out.println(responseObject.toString());
+        System.out.println("End OATH Test+++++++++++++++");
         return responseObject;
     }
 
-    private static ResponseObject testPush(SAAccess saAccess, String factorId){
-        ResponseObject responseObject = saAccess.deliverOTPByPush(user,factorId);
+    private static ResponseObject testPush(SAAccess saAccess, String factorId) {
+        ResponseObject responseObject = saAccess.deliverOTPByPush(user, factorId);
+        System.out.println("Start Push Notification +++++++++++++++++");
         System.out.println(responseObject.toString());
+        System.out.println("End Push Notification +++++++++++++++++");
         return responseObject;
     }
 
-    private static IPEval testIP(SAAccess saAccess,String userid, String ip_address){
+    private static IPEval testIP(SAAccess saAccess, String userid, String ip_address) {
         IPEval ipEval = saAccess.iPEvaluation(userid, ip_address);
-        System.out.println(ipEval.toString());
+        System.out.println("Start IPEvaluation  +++++++++++++++++");
+        if (ipEval != null) {
+            System.out.println(ipEval.toString());
+        } else {
+            System.out.println("If you See this then Something went Wrong!!");
+        }
+        System.out.println("End IPEvaluation  +++++++++++++++++");
         return ipEval;
     }
 
+    private static JSObjectResponse getJSSrc(SAAccess saAccess) {
+        System.out.println("START JavaScript Get URL Request ++++++++");
+        JSObjectResponse jsObject = saAccess.javaScriptSrc();
+        System.out.println(jsObject.getSrc());
+        System.out.println("END JavaScript Get URL Request ++++++++");
+        return jsObject;
+    }
+
+    private static ResponseObject testAccessHistories(SAAccess saAccess, String user, String ip_address) {
+        System.out.println("START Access Histories test +++++++++");
+        ResponseObject ahistories = saAccess.accessHistory(user, ip_address);
+        System.out.println(ahistories.toString());
+        System.out.println("END Access Histories test +++++++++++");
+        return ahistories;
+    }
+
+    private static void testPushToAccept(SAAccess saAccess, String user, String factorID, String ipAddress) throws InterruptedException {
+
+        System.out.println("Start Push 2 Accept Test ++++++++++++++++++");
+        ResponseObject ro = saAccess.sendPushToAcceptReq(user, factorID, ipAddress, null, null);
+        System.out.println(ro);
+        String refId = ro.getReference_id();
+        PushAcceptStatus status;
+        do {
+            Thread.sleep(2000);
+            status = saAccess.queryPushAcceptStatus(refId);
+            System.out.println(status);
+        } while ("PENDING".equals(status.getMessage()));
+        //break;
+        System.out.println("END Push 2 Accept Test+++++++++++++++++++");
+    }
+
+    private static ResponseObject testAdaptiveAuth(SAAccess saAccess, String user, String ipAddress){
+        System.out.println("Start Adaptive Test ++++++++++++++++");
+        ResponseObject responseObject = saAccess.adaptiveAuthQuery(user,ipAddress);
+        System.out.println(responseObject.toString());
+        System.out.println("End Adaptive Test ++++++++++++++++++");
+        return responseObject;
+    }
 }
