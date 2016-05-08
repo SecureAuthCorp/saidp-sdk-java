02be2a058c949bf7e7ed70a609b61b91b95750ea
sa-rrowcliffe <rrowcliffe@secureauth.com>
2016-05-08 09:21:05 -0700

    Objects to support user and group mappings

---
+++ src/main/java/org/secureauth/sarestapi/data/UserProfile/UserToGroups.java
@@ -0,0 +1,16 @@
+package org.secureauth.sarestapi.data.UserProfile;
+
+/**
+ * Created by rrowcliffe on 5/8/16.
+ */
+public class UserToGroups {
+    private String[] groupNames;
+
+    public String[] getGroupNames() {
+        return groupNames;
+    }
+
+    public void setGroupNames(String[] groupNames) {
+        this.groupNames = groupNames;
+    }
+}
---
+++ src/main/java/org/secureauth/sarestapi/data/UserProfile/UsersToGroup.java
@@ -0,0 +1,16 @@
+package org.secureauth.sarestapi.data.UserProfile;
+
+/**
+ * Created by rrowcliffe on 5/8/16.
+ */
+public class UsersToGroup {
+    private String[] userIds;
+
+    public String[] getUserIds() {
+        return userIds;
+    }
+
+    public void setUserIds(String[] userIds) {
+        this.userIds = userIds;
+    }
+}
