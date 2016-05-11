c88235186d6ace21b790375c15371bfe2deeda63
sa-rrowcliffe <rrowcliffe@secureauth.com>
2016-05-10 20:16:33 -0700

    Support for NONNulls

--- src/main/java/org/secureauth/sarestapi/data/Response/GroupAssociationResponse.java
+++ src/main/java/org/secureauth/sarestapi/data/Response/GroupAssociationResponse.java
@@ -1,11 +1,16 @@
 package org.secureauth.sarestapi.data.Response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import javax.xml.bind.annotation.XmlRootElement;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
  * Created by rrowcliffe on 5/8/16.
  */
+@XmlRootElement
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class GroupAssociationResponse extends BaseResponse {
     HashMap<String,ArrayList<String>> failures = new HashMap<>();
 
