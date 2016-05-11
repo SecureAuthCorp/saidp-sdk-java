881bbadb3d0b66567e4650397f1d635b9faf3274
sa-rrowcliffe <rrowcliffe@secureauth.com>
2016-05-10 20:34:32 -0700

    updated UserProfile Response

--- src/main/java/org/secureauth/sarestapi/data/Response/UserProfileResponse.java
+++ src/main/java/org/secureauth/sarestapi/data/Response/UserProfileResponse.java
@@ -2,10 +2,13 @@ package org.secureauth.sarestapi.data.Response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import org.secureauth.sarestapi.data.Response.BaseResponse;
-import org.secureauth.sarestapi.data.UserProfile.UserProfile;
+import org.secureauth.sarestapi.data.UserProfile.*;
 import org.secureauth.sarestapi.util.JSONUtil;
 
 import javax.xml.bind.annotation.XmlRootElement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by rrowcliffe on 5/1/16.
@@ -14,8 +17,10 @@ import javax.xml.bind.annotation.XmlRootElement;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserProfileResponse extends BaseResponse{
     private String userId;
-    private UserProfile userProfile;
-
+    private HashMap<String,UserProfileProperty> properties = new HashMap<>();
+    private HashMap<String,UserProfileKB> knowledgeBase = new HashMap<>();
+    private List<String> groups = new ArrayList<>();
+    private List<UserProfileAccessHistory> accessHistories = new ArrayList<>();
 
     public String getUserId() {
         return userId;
@@ -25,12 +30,36 @@ public class UserProfileResponse extends BaseResponse{
         this.userId = userId;
     }
 
-    public UserProfile getUserProfile() {
-        return userProfile;
+    public HashMap<String, UserProfileProperty> getProperties() {
+        return properties;
     }
 
-    public void setUserProfile(UserProfile userProfile) {
-        this.userProfile = userProfile;
+    public void setProperties(HashMap<String, UserProfileProperty> properties) {
+        this.properties = properties;
+    }
+
+    public HashMap<String, UserProfileKB> getKnowledgeBase() {
+        return knowledgeBase;
+    }
+
+    public void setKnowledgeBase(HashMap<String, UserProfileKB> knowledgeBase) {
+        this.knowledgeBase = knowledgeBase;
+    }
+
+    public List<String> getGroups() {
+        return groups;
+    }
+
+    public void setGroups(List<String> groups) {
+        this.groups = groups;
+    }
+
+    public List<UserProfileAccessHistory> getAccessHistories() {
+        return accessHistories;
+    }
+
+    public void setAccessHistories(List<UserProfileAccessHistory> accessHistories) {
+        this.accessHistories = accessHistories;
     }
 
     @Override
