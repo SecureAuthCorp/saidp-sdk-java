451999d40f84a9ce27948fb40f657390c0c69eeb
Sebastian Bertran <sbertran@secureauth.com>
2020-07-31 14:24:26 -0300

    JSI-19: new test added.

--- src/test/java/org/secureauth/restapi/test/SAAccessTDD.java
+++ src/test/java/org/secureauth/restapi/test/SAAccessTDD.java
@@ -60,7 +60,7 @@ public class SAAccessTDD {
 	@Before
 	public void setup() {
 		setupStrings();
-		Assume.assumeTrue(assumeTest);
+		//Assume.assumeTrue(assumeTest);
 		saAuth = new SAAuth(apiApplicationId, apiApplicationKey, realm);
 		saBaseURL = new SABaseURL(host, port, ssl, true);
 		saExecuter = new SAExecuter(saBaseURL);
@@ -429,18 +429,24 @@ public class SAAccessTDD {
 	@Test
 	public void testNotifySuccessAuthenticatedResult() {
 		BaseResponse response = saAccess.notifyAuthenticationResult( validUsername, "success" );
-		assertEquals( response.getMessage(), "Request has been processed." );
+		assertEquals( "Request has been processed.", response.getMessage() );
 	}
 
 	@Test
 	public void testNotifyAbortedAuthenticatedResult() {
 		BaseResponse response = saAccess.notifyAuthenticationResult( validUsername, "aborted" );
-		assertEquals( response.getMessage(), "Request has been processed." );
+		assertEquals( "Request has been processed.", response.getMessage() );
 	}
 
 	@Test
-	public void testNotifyCanceledAuthenticatedResult() {
+	public void testNotifyCancelledAuthenticatedResult() {
 		BaseResponse response = saAccess.notifyAuthenticationResult( validUsername, "cancelled" );
-		assertEquals( response.getMessage(), "Request has been processed." );
+		assertEquals( "Request has been processed.", response.getMessage() );
+	}
+
+	@Test
+	public void testWhenUserIdIsNotValidThenNotifyAuthenticatedResultFail() {
+		BaseResponse response = saAccess.notifyAuthenticationResult( UNEXISTING_USERNAME, "success" );
+		assertEquals( "User Id was not found.", response.getMessage() );
 	}
 }
