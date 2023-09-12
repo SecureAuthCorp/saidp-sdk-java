df9729e9685404dc03c9998a8849184c641463eb
Gastón Axel Lacuesta <glacuesta@secureauth.com>
2023-09-12 07:27:49 -0700

    ignore unknown fields

--- src/main/java/org/secureauth/sarestapi/data/PreferredMFA.java
+++ src/main/java/org/secureauth/sarestapi/data/PreferredMFA.java
@@ -2,29 +2,25 @@ package org.secureauth.sarestapi.data;
 
 import org.secureauth.sarestapi.util.JSONUtil;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class PreferredMFA {
 
-    private String type;
-    private String mode;
-    private String available_choosen_mode;
+    private String factorId;
+    private String capability;
 
-    public String getType() {
-        return type;
+    public String getFactorId() {
+        return factorId;
     }
-    public void setType(String type) {
-        this.type = type;
+    public void setFactorId(String factorId) {
+        this.factorId = factorId;
     }
-    public String getMode() {
-        return mode;
+    public String getCapability() {
+        return capability;
     }
-    public void setMode(String mode) {
-        this.mode = mode;
-    }
-    public String getAvailable_choosen_mode() {
-        return available_choosen_mode;
-    }
-    public void setAvailable_choosen_mode(String available_choosen_mode) {
-        this.available_choosen_mode = available_choosen_mode;
+    public void setCapability(String capability) {
+        this.capability = capability;
     }
     @Override
     public String toString(){
