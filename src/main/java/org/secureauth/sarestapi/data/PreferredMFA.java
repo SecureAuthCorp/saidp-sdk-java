9b76c092fc091bca73c7b4f8f9a6a66f6cdbd1a4
glacuesta <89663911+glacuesta-sa@users.noreply.github.com>
2023-09-28 19:29:07 -0300

    RAD-663: Implement Preferred MFA feature (#128)
    RAD-663: Implement Preferred MFA feature
    * ignore unknow fields on responses to avoid crashes

--- src/main/java/org/secureauth/sarestapi/data/PreferredMFA.java
+++ src/main/java/org/secureauth/sarestapi/data/PreferredMFA.java
@@ -2,8 +2,15 @@ package org.secureauth.sarestapi.data;
 
 import org.secureauth.sarestapi.util.JSONUtil;
 
-public class PreferredMFA {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PreferredMFA {
+	 
+	private String factorId;
+	private String capability;
     private String type;
     private String mode;
     private String available_choosen_mode;
@@ -26,6 +33,20 @@ public class PreferredMFA {
     public void setAvailable_choosen_mode(String available_choosen_mode) {
         this.available_choosen_mode = available_choosen_mode;
     }
+    
+    public String getFactorId() {
+        return factorId;
+    }
+    public void setFactorId(String factorId) {
+        this.factorId = factorId;
+    }
+    public String getCapability() {
+        return capability;
+    }
+    public void setCapability(String capability) {
+        this.capability = capability;
+    }
+    
     @Override
     public String toString(){
         return JSONUtil.convertObjectToJSON(this);
